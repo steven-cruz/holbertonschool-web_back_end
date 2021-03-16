7264ec1c687cbacb28cb65257d274d3c0f751e8d
steven-cruz <stevencruz.300008@gmail.com>
2021-03-15 19:03:55 -0500

    Basic annotations - contact

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+takes two arguments (string) and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function to concat two strings
+    """
+    return str1 + str2
