ab3cc1ca00d1d399868e61e0379bcf4af67c704d
steven-cruz <stevencruz.300008@gmail.com>
2021-03-22 20:24:09 -0500

    Basic dictionary

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -3,7 +3,10 @@
     Create a class BasicCache that inherits from BaseCaching
     and is a caching system.
 """
+
 BaseCaching = __import__('base_caching').BaseCaching
+
+
 class BasicCache(BaseCaching):
     ''' A basic cache.
         Inherits from class BaseCaching.
