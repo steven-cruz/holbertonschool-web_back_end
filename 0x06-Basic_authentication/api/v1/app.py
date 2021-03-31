7c5af8a6bd5e182c98e5a32541785f73530da7ab
steven-cruz <stevencruz.300008@gmail.com>
2021-03-31 02:48:47 -0500

    Error handler:Unauthorized

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -20,6 +20,12 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+        Request unauthorized
+    """
+    return jsonify({"error": "Unauthorized"})
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
