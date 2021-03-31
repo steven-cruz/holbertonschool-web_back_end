3aa57442a00deb9d54da434de1cb46be55b6161b
steven-cruz <stevencruz.300008@gmail.com>
2021-03-30 22:21:49 -0500

    Regex-ing

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+import logging
+from os import environ
+import re
+from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
