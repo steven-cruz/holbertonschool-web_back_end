b8714dd6e6483a6dc8e1be180d7aa5dadee4ce4e
steven-cruz <stevencruz.300008@gmail.com>
2021-04-19 15:34:53 -0500

    Udapte user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -62,3 +62,20 @@ class DB:
             raise NoResultFound
 
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+            use the find_user_by method to search
+            and update the data passed in the method
+        """
+        user = self.find_user_by(id=user_id)
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise ValueError
+
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
+        self._session.commit()
