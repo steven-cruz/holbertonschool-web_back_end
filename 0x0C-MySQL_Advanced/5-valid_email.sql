ccfec0bdd80a7146ea7b6d1ceb5a5585527c29e7
steven-cruz <stevencruz.300008@gmail.com>
2021-04-29 23:49:16 -0500

    Email Validation to sent

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Email validation to sent.
+-- Script that creates a trigger that resets the attribute
+-- valid_email only when the email has been changed.
+DELIMITER $$
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF NEW. email <> OLD.email
+THEN
+    SEND NEW.valid_email = 0;
+END IF;
+END
+$$
+DELIMITER;
