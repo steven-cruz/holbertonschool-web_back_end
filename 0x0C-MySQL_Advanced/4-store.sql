705e2f7de095991423a2814f6578ed2213be4b79
steven-cruz <stevencruz.300008@gmail.com>
2021-04-29 23:25:31 -0500

    Buy buy buy

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -4,4 +4,4 @@
 CREATE TRIGGER buy_trigger
 AFTER INSERT ON orders
 FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.item_name;
+UPDATE items SET quantity = quantity - NEW.number WHERE  name = NEW.item_name;
