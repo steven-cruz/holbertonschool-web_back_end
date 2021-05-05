6069e4092d2cf9accae4d23970aa85f57d7594a7
steven-cruz <stevencruz.300008@gmail.com>
2021-05-04 23:33:46 -0500

    log stats

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+    Log stats
+"""
+
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ Database: logs
+        Collection: nginx
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print("{} status check".format(status_check))
